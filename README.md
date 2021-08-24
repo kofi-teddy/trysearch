8279f7b1d1c24d7b8e0dfb3d5b71773d53827a0f
kofi-teddy <teddyagud@teddys-MacBook-Pro.local>
2021-08-24 01:02:38 +0000

    reamde updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # trysearch
+
+
+
+### Todos
+- __contains
+- __iscontains
+- __search
+- SearchVector
+- SearchRank
+- SearchRank Weights
+- TrigramSimilarity & TrigramDistance
+- Headline
+- Build a Gin index and test performance
---
+++ apps/book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import gettext as _ 
+from django.contrib.postgres.indexes import GinIndex
+
+
+class Book(models.Model):
+    title = models.CharField(_("title"), max_length=1000, null=False, db_index=True)
+    authors = models.CharField(_("author"), max_length=1000) 
+
+    class Meta:
+        indexes = [
+            GinIndex(name="NewGinIndex", fields=["title"], opclasses=["gin_trgm_ops"])
+        ]
